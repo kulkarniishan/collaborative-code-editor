40e7d07700b654434284f2b5bf99acb1c957b645
Anurag Ghosh <anurag.ghosh@somaiya.edu>
2022-01-17 16:38:50 +0530

    FEATURE: Frontend initialization

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline bg-neutral-400">
+      Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
