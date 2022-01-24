8e4e32306d48dd9fcb15c834e115cae3d565a1f2
Anurag Ghosh <anurag.ghosh@somaiya.edu>
2022-01-24 18:23:18 +0530

    FEATURE: Added the login and Sigin Components

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline bg-neutral-400">
-      Hello world!
-      </h1>
+      <div className="">
+        
+      </div>
     </div>
   );
 }
