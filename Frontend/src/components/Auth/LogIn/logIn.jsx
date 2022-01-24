79ea3b286a0434a8dcc0b07d9dd6c189bbb596ab
Anurag Ghosh <anurag.ghosh@somaiya.edu>
2022-01-24 18:51:31 +0530

    FEATURE: Added the frontend skeleton

---
+++ Frontend/src/components/Auth/LogIn/logIn.jsx
@@ -0,0 +1,16 @@
+import * as yup from 'yup'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { FaTimesCircle } from 'react-icons/fa'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+
+function LogIn() {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+}
+
+export default LogIn
