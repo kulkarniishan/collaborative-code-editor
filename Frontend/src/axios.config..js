2b8d93ee2437dbee644bb4eadff1b4565decd8c3
Ishan <ishan.kulkarni@somaiya.edu>
2022-01-29 01:25:50 +0530

    FEATURE: Axios config file added

---
+++ Frontend/src/axios.config..js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const baseURL = 'http://localhost:5000'
+//export const baseURL = 'http://ec2-3-95-176-91.compute-1.amazonaws.com'
+axios.defaults.withCredentials = true;
+
+export const axiosInstance = axios.create({
+	baseURL: baseURL,
+	timeout: 300000,
+});
